ab95bd7a88a2d5842c93efddcea0f35906aad94a
Piotr <piotr.blazejak@yahoo.com>
2024-07-02 23:56:13 +0200

    created styles

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#07071E",
+        foreground: "#BECCE9",
+        primary: "#E84646",
+        secondary: "#EAF1FF",
+      },
+    },
+  },
+};
